74d41a0a49b61b2191621a0ef368aab041d42746
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-05-26 04:41:44 +0900

    config file

--- docs/edit_announce.json.php
+++ docs/edit_announce.json.php
@@ -8,6 +8,7 @@ header('X-Powered-By: Hidden');
 
 $_SESSION['user_id']=isset($_SESSION['user_id'])?$_SESSION['user_id']:null;
 
+$config['internal']['config']['filename'] = '../.secret/config.json';
 $config['internal']['announce']['file']['path']=__DIR__.'/../discord_json_announcements/_announce.json';
 $config['internal']['announce']['file']['path']=is_file($config['internal']['announce']['file']['path'])?realpath($config['internal']['announce']['file']['path']):'';
 $config['internal']['jsonparse']['encode']=JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_INVALID_UTF8_IGNORE|JSON_INVALID_UTF8_SUBSTITUTE|JSON_THROW_ON_ERROR;
