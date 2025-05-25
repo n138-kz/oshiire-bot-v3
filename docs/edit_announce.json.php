bed63dc5f00483676b27063b0fe9a9b50f3aa80c
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-05-26 04:41:52 +0900

    config file file exist

--- docs/edit_announce.json.php
+++ docs/edit_announce.json.php
@@ -9,6 +9,17 @@ header('X-Powered-By: Hidden');
 $_SESSION['user_id']=isset($_SESSION['user_id'])?$_SESSION['user_id']:null;
 
 $config['internal']['config']['filename'] = '../.secret/config.json';
+if(!file_exists($config['internal']['config']['filename'])){
+	http_response_code(500);
+	$result['result']=[
+		'id'=>1,
+		'level'=>'Fatal',
+		'description'=>'Config load failed: No such file or directory: `'.$config['internal']['config']['filename'].'`',
+	];
+	error_log($result['result']['level'].': '.$result['result']['description'].' evented on '.__FILE__.'#'.__LINE__);
+	echo json_encode($result['result'],JSON_PRETTY_PRINT|JSON_INVALID_UTF8_IGNORE|JSON_UNESCAPED_UNICODE);
+	exit(1);
+}
 $config['internal']['announce']['file']['path']=__DIR__.'/../discord_json_announcements/_announce.json';
 $config['internal']['announce']['file']['path']=is_file($config['internal']['announce']['file']['path'])?realpath($config['internal']['announce']['file']['path']):'';
 $config['internal']['jsonparse']['encode']=JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_INVALID_UTF8_IGNORE|JSON_INVALID_UTF8_SUBSTITUTE|JSON_THROW_ON_ERROR;
