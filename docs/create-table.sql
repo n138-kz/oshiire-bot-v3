5c65cd6b2e5850840941f8344874fc46f8334f0e
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-06-10 15:59:33 +0900

    sql create table oshiirebotv3_contentjson

--- docs/create-table.sql
+++ docs/create-table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE IF NOT EXISTS oshiire-bot-v3 (
-  "timestamp" double precision NOT NULL,
-  uuid text NOT NULL,
-  client_address text NOT NULL,
+CREATE TABLE IF NOT EXISTS oshiirebotv3_contentjson (
+  "timestamp" double precision NOT NULL DEFAULT EXTRACT(epoch FROM CURRENT_TIMESTAMP),
+  uuid text NOT NULL, -- $_SERVER['UUID']
+  client_address text NOT NULL, -- $_SERVER['REMOTE_ADDR']
   client_name text NOT NULL,
   request text NOT NULL,
-  external_id text NOT NULL,
+  external_id text NOT NULL, -- Discord user-id
   content_json_before text NOT NULL,
   content_json_after text NOT NULL,
-  CONSTRAINT isjp_pkey PRIMARY KEY (uuid)
+  CONSTRAINT oshiirebotv3_contentjson_pkey PRIMARY KEY (uuid)
 );
