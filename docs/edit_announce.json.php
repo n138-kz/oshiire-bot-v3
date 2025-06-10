76f5a37ffc9e0451adeeb936975b339602766ba2
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-06-10 14:32:46 +0900

    $config['internal']['sessions']['file']['path']

--- docs/edit_announce.json.php
+++ docs/edit_announce.json.php
@@ -38,6 +38,9 @@ if(!is_readable($config['internal']['config']['filename'])){
 $config = array_merge($config, json_decode(file_get_contents($config['internal']['config']['filename']), TRUE));
 $config['internal']['announce']['file']['path']=__DIR__.'/../discord_json_announcements/_announce.json';
 $config['internal']['announce']['file']['path']=is_file($config['internal']['announce']['file']['path'])?realpath($config['internal']['announce']['file']['path']):'';
+$config['internal']['sessions']['file']['path']=__DIR__.'/../discord_json_announcements/_discord-sessions.txt';
+$config['internal']['sessions']['file']['path']=is_file($config['internal']['sessions']['file']['path'])?realpath($config['internal']['sessions']['file']['path']):'';
+
 $config['internal']['jsonparse']['encode']=JSON_PRETTY_PRINT|JSON_NUMERIC_CHECK|JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE|JSON_INVALID_UTF8_IGNORE|JSON_INVALID_UTF8_SUBSTITUTE|JSON_THROW_ON_ERROR;
 $config['internal']['redirect']['url']=$_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].preg_replace('/\.php$/', '.html', $_SERVER['PHP_SELF']);
 $config['internal']['authz']=(isset($config['internal']['authz']))?$config['internal']['authz']:[];
