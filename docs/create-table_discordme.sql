3a5ca4d35f7732bf9b1767853966ce388ef8cab1
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2025-07-19 22:55:03 +0900

    Update create-table_discordme.sql

--- docs/create-table_discordme.sql
+++ docs/create-table_discordme.sql
@@ -20,3 +20,33 @@ CREATE TABLE IF NOT EXISTS oshiirebotv3_discordme (
   CONSTRAINT oshiirebotv3_discordme_pkey PRIMARY KEY (userid)
 );
 ALTER TABLE IF EXISTS oshiirebotv3_discordme OWNER to webapp;
+ALTER TABLE IF EXISTS oshiirebotv3_discordme
+  ADD CONSTRAINT oshiirebotv3_discordme_premium_type_fkey FOREIGN KEY (premium_type)
+  REFERENCES discord_premium_type (id) MATCH SIMPLE;
+DROP VIEW IF EXISTS oshiirebotv3_discordme_view;
+CREATE OR REPLACE VIEW oshiirebotv3_discordme_view
+  AS
+  SELECT
+    to_timestamp(trunc(oshiirebotv3_discordme."timestamp")) as timestamp,
+    oshiirebotv3_discordme.userid,
+    oshiirebotv3_discordme.username,
+    oshiirebotv3_discordme.global_name,
+    oshiirebotv3_discordme.avatar,
+    oshiirebotv3_discordme.discriminator,
+    oshiirebotv3_discordme.public_flags,
+    oshiirebotv3_discordme.flags,
+    oshiirebotv3_discordme.banner,
+    oshiirebotv3_discordme.accent_color,
+    oshiirebotv3_discordme.avatar_decoration_data,
+    oshiirebotv3_discordme.collectibles,
+    oshiirebotv3_discordme.banner_color,
+    oshiirebotv3_discordme.clan,
+    oshiirebotv3_discordme.primary_guild,
+    oshiirebotv3_discordme.locale,
+    discord_premium_type.description as premium_type
+  FROM oshiirebotv3_discordme
+  INNER JOIN discord_premium_type
+	ON
+    oshiirebotv3_discordme.premium_type = discord_premium_type.id
+  ORDER BY
+    oshiirebotv3_discordme."timestamp" DESC;
